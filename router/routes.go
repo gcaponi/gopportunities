cb6a64c371d527310e391df3a219ed2ede33f08b
gcaponi <guilherme.caponi@gmail.com>
2024-03-15 18:42:59 +0100

    update handler structure

--- router/routes.go
+++ router/routes.go
@@ -1,43 +1,17 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/gcaponi/gopportunities/router/handler"
 	"github.com/gin-gonic/gin"
 )
 
-func InitializeRoutes(router *gin.Engine){
+func initializeRoutes(router *gin.Engine){
 	v1 := router.Group( "/api/v1") 
 	{
-		// Show Opening
-		v1.GET("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message":"GET Opening",
-			})
-		})
-		// Post Opening
-		v1.POST("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message":"POST Opening",
-			})
-		})
-		// Delete Opening
-		v1.DELETE("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message":"DELETE Opening",
-			})
-		})
-		// UpDating Opening
-		v1.PUT("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-			"message":"PUT Opening",
-			})
-		})
-		// Show Openings
-		v1.GET("/openings", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-			"message":"GET Openings",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
