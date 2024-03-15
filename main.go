61ccc1f8e58cec3f53e91122e8540f55decfdb49
gcaponi <guilherme.caponi@gmail.com>
2024-03-15 20:46:27 +0100

    add open schema, logger

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/gcaponi/gopportunities/config"
 	"github.com/gcaponi/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
-	// Inizializzare Router
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		logger.Errorf("Config Initialization error: %v", err)
+	return
+	}
+
+	// Initialize Router
 	router.Initialize()
 
 }
