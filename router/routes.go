1e6e91c1702b75ba2e4e7e2b9a949f455fef8ee5
gcaponi <guilherme.caponi@gmail.com>
2024-03-15 18:11:30 +0100

    routes

--- router/routes.go
+++ router/routes.go
@@ -7,12 +7,37 @@ import (
 )
 
 func InitializeRoutes(router *gin.Engine){
-	v1 := router.Group( "/api/v1")
+	v1 := router.Group( "/api/v1") 
 	{
+		// Show Opening
 		v1.GET("/opening", func(ctx *gin.Context){
 			ctx.JSON(http.StatusOK, gin.H{
 				"message":"GET Opening",
 			})
 		})
+		// Post Opening
+			v1.POST("/opening", func(ctx *gin.Context){
+				ctx.JSON(http.StatusOK, gin.H{
+					"message":"POST Opening",
+				})
+			})
+		// Delete Opening
+			v1.DELETE("/opening", func(ctx *gin.Context){
+				ctx.JSON(http.StatusOK, gin.H{
+					"message":"DELETE Opening",
+				})
+			})
+		// UpDating Opening
+			v1.PUT("/opening", func(ctx *gin.Context){
+				ctx.JSON(http.StatusOK, gin.H{
+				"message":"PUT Opening",
+				})
+			})
+		// Show Openings
+			v1.GET("/opening", func(ctx *gin.Context){
+				ctx.JSON(http.StatusOK, gin.H{
+				"message":"GET Openings",
+				})
+			})
 	}
 }
